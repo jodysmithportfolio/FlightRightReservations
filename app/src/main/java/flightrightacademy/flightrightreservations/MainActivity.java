be4fc750f2f895e863a7e9e017b047b5286f9567
iagospeare <jodysmith715@gmail.com>
2016-06-25 19:55:56 -0400

    Initial commit

---
+++ app/src/main/java/flightrightacademy/flightrightreservations/MainActivity.java
@@ -0,0 +1,16 @@
+package flightrightacademy.flightrightreservations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
